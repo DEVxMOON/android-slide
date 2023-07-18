6b7b7deaa369c165f74f39ec8d4bae66b3e822b5
Harri0tJ <dev.muun07@gmail.com>
2023-07-18 09:28:41 +0900

    basic class upload - Slide.kt : basic view attribute - Square.kt : square view attribute (with Slide) - RGB.kt : RGB attribute - MainActivity.kt : test print - activity_main.xml(sw600dp) : tablet layout

--- app/src/main/java/com/hr/slide_app/MainActivity.kt
+++ app/src/main/java/com/hr/slide_app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.hr.slide_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val slideFactory = SquareSlideFactory()
+
+        val rect1 = slideFactory.create(216,RGB(245,0,245),9)
+        val rect2 = slideFactory.create(384,RGB(43,124,95),5)
+        val rect3 = slideFactory.create(108,RGB(98,244,15),7)
+        val rect4 = slideFactory.create(233,RGB(125,39,99),1)
+
+        Log.d("Square", "Rect1 $rect1")
+        Log.d("Square", "Rect2 $rect2")
+        Log.d("Square", "Rect3 $rect3")
+        Log.d("Square", "Rect4 $rect4")
+
     }
 }
