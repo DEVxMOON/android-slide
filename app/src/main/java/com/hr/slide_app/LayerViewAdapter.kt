6398d1f96ef15cca1066ec80dedcacf9f510d971
Harri0tJ <dev.muun07@gmail.com>
2023-07-27 11:56:36 +0900

    [update & upload] recycler view update
    change recycler view item type by slide type.
    Use Long click to make popup menu(and it works!)
    - LayerViewAdapter.kt
    - layer_layout.xml
    - slide_menu.xml

--- app/src/main/java/com/hr/slide_app/LayerViewAdapter.kt
+++ app/src/main/java/com/hr/slide_app/LayerViewAdapter.kt
@@ -2,6 +2,7 @@ package com.hr.slide_app
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.hr.slide_app.databinding.LayerLayoutBinding
@@ -18,10 +19,53 @@ class LayerViewAdapter : RecyclerView.Adapter<LayerViewAdapter.LayerViewHolder>(
 
     inner class LayerViewHolder(private val binding: LayerLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.layerItem.setOnLongClickListener {
+                val pop = PopupMenu(binding.root.context, it)
+                pop.inflate(R.menu.slide_menu)
+
+                pop.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.bring_to_front -> {
+                            changeSlideOrder(layoutPosition, 0)
+                            return@setOnMenuItemClickListener true
+                        }
+
+                        R.id.bring_forward -> {
+                            changeSlideOrder(layoutPosition, layoutPosition - 1)
+                            return@setOnMenuItemClickListener true
+                        }
+
+                        R.id.send_backward -> {
+                            changeSlideOrder(layoutPosition, layoutPosition + 1)
+                            return@setOnMenuItemClickListener true
+                        }
+
+                        R.id.send_to_back -> {
+                            changeSlideOrder(layoutPosition, layerList.lastIndex)
+                            return@setOnMenuItemClickListener true
+                        }
+
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                }
+                pop.show()
+                true
+            }
+        }
+
         fun bind(pos: Int, slide: Slide) {
             val idx = pos + 1
             binding.txtNumbering.text = idx.toString()
-            binding.type.setImageResource(R.drawable.checkbox_indeterminate_filled_icon)
+            if(slide.type==0)
+            {
+                binding.type.setImageResource(R.drawable.square_png)
+            }
+            else if(slide.type==1)
+            {
+                binding.type.setImageResource(R.drawable.image_png)
+            }
 
             binding.root.setOnClickListener { listener?.onLayerClicked(slide) }
             if (pos == selectedIndex) {
